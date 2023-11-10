5ab57be051f34e52413473598cd7fc2a455257d0
amezianechayer <amezianechayer@github.com>
2023-11-10 22:36:36 +0100

    feat: :zap: Added prisma with MySQL

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,12 +2,15 @@
 
 import React, { useState } from "react";
 import SearchBar from "./SearchBar";
-
+import Link from "next/link";
+import {CiShoppingCart} from "react-icons/ci"
+import {BsChevronCompactUp} from "react-icons/bs"
+import { BiSearch } from "react-icons/bi";
 type Props = {}
 
 const Navbar = (props: Props) => {
     const [showProfile, setShowProfile] = useState<boolean>(false)
-    const [showNav, setSowNav] = useState<boolean>(false)
+    const [showNav, setShowNav] = useState<boolean>(false)
     return (
         <div>
             <div className="flex items-center justify-between py-4 relative">
@@ -23,6 +26,36 @@ const Navbar = (props: Props) => {
                 </div>
                 <div className="flex items-center space-x-4">
                     <SearchBar />
+                    <div onClick={() => setShowProfile(!showProfile)} className="relative cursor-pointer">
+                        <img src="user.png" className="w-[35px] h-[35px] rounded-full object-cover" alt=""/>
+                        <div className={`absolute bg-white z-[2] rounded-lg shadow-lg ${showProfile ? "":"hidden"}`}>
+                            <Link href="/sign">SignIn</Link>
+                        </div>
+                        
+                    </div>
+                    <Link href="/cart">
+                            <div className="p-2 bg-gray-100 rounded-full"><CiShoppingCart size={20} /></div>
+                        </Link>
+                        <span onClick={() => setShowNav(!showNav)} className="p-[9px] bg-gray-100 rounded-full md:hidden">
+                            <BsChevronCompactUp className={`transition ease-in duration-150 ${showNav ? "rotate-180":"0"}`}/>
+                        </span>
+                </div>
+            </div>
+            <div className={`md:hidden ${showNav ? "pb-4 px-5": "h-0 invisible opacity-0"}`}>
+                <ul className="flex flex-col text-[15px] opacity-75 px-2">
+                    <li><a href="/shop" className="py-3 inline-block w-full">Shop</a></li>
+                    <li><a href="/filters" className="py-3 inline-block w-full">Filters</a></li>
+                    <li><a href="/myproducts" className="py-3 inline-block w-full">My Products</a></li>
+
+                </ul>
+                <div className="flex items-center bg-gray-100 p-2 rounded-lg my-4 py-3">
+                    <input
+                    type="text"
+                    className="outline-none w-full bg-transparent ml-2 caret-blue-500 placeholder:font-light placeholder:text-gray-600 text-[15px]"
+                    placeholder="Serach"
+                    autoComplete="false"
+                    />
+                    <button><BiSearch size={20} className="opacity-50"/></button>
                 </div>
             </div>
         </div>
--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -9,6 +9,12 @@ const SearchBar = (props: Props) => {
         <div>
             <div className="flex items-center bg-gray-100 p-2 rounded-full max-md:hidden">
                 <button><BiSearch size={20} className="opacity-50"/></button>
+                <input
+                type="text"
+                className="outline-none bg-transparent ml-2 caret-blue-500 placeholder:font-light placeholder:text-gray-600 text-[15px]"
+                placeholder="Serach"
+                autoComplete="false"
+                />
             </div>
         </div>
     )
