a4952ffd6a0e3d732d1b70bb4db86145c74881bb
amezianechayer <amezianechayer@github.com>
2023-11-11 01:28:30 +0100

    feat: :sparkles: Added SignUp connexion

---
+++ app/signin/signinform.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import React, {useState} from "react";
+import Link from "next/link"
+import {useRouter} from "next/navigation"
+import axios from "axios";
+import {signIn} from "next-auth/react"
+
+type Props = {}
+
+const Signinform = (props: Props) => {
+    return (
+        <div>S</div>
+    )
+}
+
+export default Signinform 
